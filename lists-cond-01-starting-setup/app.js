03f54dd6572ec1186742944f8c0a870bfbd45cde
Son Bui <ngocson.bui0312@gmail.com>
2024-05-06 16:34:48 +0200

    v-if-else

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,25 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: [],
+      currentGoal: "",
+      // display: "",
+    };
+  },
+  watch: {},
+  computed: {
+    // display() {
+    //   return this.goals.length === 0
+    //     ? "No goals have been added yet - please start adding some!"
+    //     : "";
+    // },
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.currentGoal);
+      this.currentGoal = "";
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
